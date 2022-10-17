2abf35cf53e0ff61cb789413bcc234588f2cc99a
Azure <marleyalmere@gmail.com>
2022-10-17 18:09:03 +0200

    Update
    did some work Game is almost finished.

--- resources/app.js
+++ resources/app.js
@@ -8,6 +8,7 @@ const playVplay = document.getElementById('playText2')
 const container1 = document.getElementById('container')
 const container2 = document.getElementById('container2')
 
+
 chooseO2.addEventListener('click', e => {
     chooseO.style.display = 'block'
     chooseO2.style.display = 'none'
@@ -33,6 +34,8 @@ playVplay.addEventListener('click', e => {
     container1.style.display = 'none'
     container2.style.display = 'block'
     game()
+    
+
 })
 
 
@@ -42,6 +45,17 @@ const restartbtn = document.getElementById('restart')
 const turnX = document.getElementById('turnX')
 const turnO = document.getElementById('turnO')
 const card = document.getElementById('card1')
+const cardTitle = document.getElementById('takeRound');
+const xRoundImg = document.getElementById('xRound');
+const oRoundImg = document.getElementById('oRound');
+const playerScore = document.getElementById('playerScore');
+const tieScore = document.getElementById('tiesScore');
+const npcScore = document.getElementById('npcScore');
+const quitBTN = document.getElementById('quiteBTN')
+const continueBTN = document.getElementById('continueBTN')
+
+ 
+
 
 const winConditions = [
     [0, 1, 2],
@@ -53,41 +67,32 @@ const winConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-
-let board = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X"
-let playerScore = 0
-let tieScore = 0
-let CPUScore = 0
-
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
 let running = false;
 
 
-function game() {
-    cells.forEach(cell => cell.addEventListener('click', cellClicked));
-    restartbtn.addEventListener('click', restartGame);
+function game(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartbtn.addEventListener("click", restartGame);
     running = true;
 }
-
 function cellClicked(){
     const cellIndex = this.getAttribute("cellIndex");
 
-    if(board[cellIndex] != "" || !running){
+    if(options[cellIndex] != "" || !running){
         return;
     }
 
-    updateCell(this, cellIndex)
-    changePlayer()
-    checkWinner()
+    updateCell(this, cellIndex);
+    checkWinner();
 }
-
 function updateCell(cell, index){
-    board[index] = currentPlayer;
-    cell.innerHTML = currentPlayer;
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
 }
-
 function changePlayer(){
-    currentPlayer = (currentPlayer === "X") ? "O" : "X";
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
     if(currentPlayer === 'O'){
         turnX.style.display = 'none'
         turnO.style.display = 'block'
@@ -98,37 +103,89 @@ function changePlayer(){
         turnO.style.display = 'none'
         
     }
-   
 }
-
 function checkWinner(){
     let roundWon = false;
 
-    for(i = 0; i < winConditions.length; i++){
+    for(let i = 0; i < winConditions.length; i++){
         const condition = winConditions[i];
-        const cellA = board(condition[0]);
-        const cellB = board(condition[1]);
-        const cellC = board(condition[2]);
-       
-       
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
 
-        if(cellA == "" || cellB == "" || cellC == "" ){
+        if(cellA == "" || cellB == "" || cellC == ""){
             continue;
-        }   
+        }
         if(cellA == cellB && cellB == cellC){
             roundWon = true;
             break;
-        } 
+        }
     }
 
     if(roundWon){
-        card.style.display = 'flex'
+        if(currentPlayer == "O"){
+            card.style.display = 'flex'
+            cardTitle.style.color = '#F2B137'
+            xRoundImg.style.display = 'none'
+            oRoundImg.style.display = 'block'
+            QuitBTN()
+            ContinueBTN()
+
+        } else if (currentPlayer == "X"){
+            card.style.display = 'flex'
+            cardTitle.style.color = '#31C3BD'
+            xRoundImg.style.display = 'block'
+            oRoundImg.style.display = 'none'
+            QuitBTN()
+            ContinueBTN()
+
+        }
         running = false;
     }
-   
-}
+    else if(!options.includes("")){
+            card.style.display = 'flex'
+            cardTitle.style.color = '#A8BFC9'
+            cardTitle.innerHTML = 'ROUND TIED'
+            xRoundImg.style.display = 'none'
+            oRoundImg.style.display = 'none'
+            QuitBTN()
+            ContinueBTN()
 
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
 function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+    card.style.display = 'none'
+} 
 
+function QuitBTN() {
+        quitBTN.addEventListener('click', e =>{
+        currentPlayer = "X";
+        options = ["", "", "", "", "", "", "", "", ""];
+        cells.forEach(cell => cell.textContent = "");
+        running = true;
+        card.style.display = 'none'
+        container1.style.display = 'flex'
+        container2.style.display = 'none'
+
+    })
 }
 
+function ContinueBTN(){
+    continueBTN.addEventListener('click', e => {
+        currentPlayer = "X";
+        options = ["", "", "", "", "", "", "", "", ""];
+        cells.forEach(cell => cell.textContent = "");
+        running = true;
+        card.style.display = 'none'
+        
+    })
+
+}
